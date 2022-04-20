a7c356391ba4159c045452f50acf558f413b5d2b
Magical <magicalmouse39@gmail.com>
2022-04-20 23:04:42 +0200

    Add map style change

--- MarillOMeter/MainPage.xaml.cs
+++ MarillOMeter/MainPage.xaml.cs
@@ -7,24 +7,37 @@ using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Maps;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// Il modello di elemento Pagina vuota è documentato all'indirizzo https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x410
-
 namespace MarillOMeter
 {
     /// <summary>
-    /// Pagina vuota che può essere usata autonomamente oppure per l'esplorazione all'interno di un frame.
+    /// Main and unique page of the application
     /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
+
+            this.FileExitBtn.Click += (s, e) =>
+                Application.Current.Exit();
+
+            foreach (var style in Enum.GetValues(typeof(MapStyle)) as MapStyle[])
+            {
+                if (style == MapStyle.None || style == MapStyle.Custom)
+                    continue;
+
+                MenuFlyoutItem styleBtn = new MenuFlyoutItem() { Text = style.ToString() };
+                styleBtn.Click += (s, e) =>
+                    this.Map.Style = style;
+                this.MapStyleBtn.Items.Add(styleBtn);
+            }
         }
     }
 }
